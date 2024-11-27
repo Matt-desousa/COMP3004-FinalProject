eb66793001de1c8ead6d6fe8b0149b7938dddd55
rachelwongcl <119693648+rachelwongcl@users.noreply.github.com>
2024-11-27 01:58:19 -0500

    Revert "Implemented data collection and skin contact functionality"
    This reverts commit bb729437fc233ac949c4542209afe56735e6aa7d.

--- mainwindow.h
+++ mainwindow.h
@@ -2,13 +2,7 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
-<<<<<<< Updated upstream
 #include "battery.h"
-=======
-#include <QMap>
-#include <QPair>
-#include <QDebug>
->>>>>>> Stashed changes
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
@@ -24,13 +18,6 @@ public:
 
 private:
     Ui::MainWindow *ui;
-<<<<<<< Updated upstream
     Battery* battery;
-=======
-    QMap<QString,QPair<int,int>> ranges; //ranges for each spot(min,max)
-    bool lastState;
-    void handleCheckboxToggled(bool checked);// skin contact
-
->>>>>>> Stashed changes
 };
 #endif // MAINWINDOW_H
